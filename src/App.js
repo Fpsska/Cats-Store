6b99e86c4829cb95091a587a7c9cf66a57f9eeb5
Fpsska <fpsska1337@gmail.com>
2021-10-08 22:28:37 +0300

    feat: add Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import './App.scss';
+import './assets/scss/style.scss';
+import Header from './components/Header/Header.jsx';
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello World</h1>
+      <Header></Header>
     </div>
   );
 }
