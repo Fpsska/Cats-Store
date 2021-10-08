00561d18c6dcf83f00076b743862becfade43c6f
Fpsska <fpsska1337@gmail.com>
2021-10-08 22:53:41 +0300

    feat: add Footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.scss';
 import './assets/scss/style.scss';
 import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Header/>
+      <main></main>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( 
+        <footer className="footer">
+                <div className="container">
+                    <div className="footer__wrapper">
+                <div className="footer__description">
+                    <h2 className="footer__title">Успей купить!</h2>
+                    <p className="footer__subtitle">Подпишись и успей словить все акции</p>
+                </div>
+                    <form className="form">
+                        <div className="form__controls">
+                        <input className="form__input" type="email" placeholder="Email" />
+                        <button className="form__button">Подписаться</button>
+                        </div>
+                        <label className="form__cheakbox-text">Подписаться на новости
+                            <input className="form__cheakbox-input" type="checkbox" />
+                        </label>
+                    </form>
+                </div>
+                </div>
+        </footer>
+    )
+}
+
+
+export default Footer;
