70cdfae024ab96fe0da2be4f5bf9dcb86ec22eb7
Fpsska <fpsska1337@gmail.com>
2021-10-08 23:05:56 +0300

    feat: add main section

--- src/App.js
+++ src/App.js
@@ -6,9 +6,29 @@ import Footer from './components/Footer/Footer.jsx';
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <main></main>
-      <Footer/>
+      <Header />
+      <main>
+        <div className="container">
+          <div className="controls">
+            <span className="controls__title">Сортировать по:</span>
+            <select className="controls__menu ">
+              <option>Цене</option>
+              <option>100</option>
+              <option>200</option>
+              <option>300</option>
+            </select>
+            <select className="controls__menu controls__menu_age">
+              <option>Возрасту</option>
+              <option>2 мес.</option>
+              <option>5 мес.</option>
+              <option>1 год</option>
+            </select>
+          </div>
+
+
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
